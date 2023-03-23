82cd16af05443aa913ea896d197ca2692ddc9d38
LoloLeMono <rouquairolr@gmail.com>
2023-03-23 10:34:04 +0100

    Ajout creationBase.sh

---
+++ creationBase.sh
@@ -0,0 +1,2 @@
+mongoimport --db COVOITURACE --collection utilisateurs --file internautes.json --jsonArray --drop
+mongoimport --db COVOITURACE --collection trajets --file covoiturages.json --jsonArray --drop

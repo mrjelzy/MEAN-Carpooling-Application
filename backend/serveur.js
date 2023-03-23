c71acc518effa527b3344c3e448c9d8bf6b50201
LoloLeMono <rouquairolr@gmail.com>
2023-03-23 11:30:40 +0100

    Ajout post

--- backend/serveur.js
+++ backend/serveur.js
@@ -1,56 +1,49 @@
+const { json } = require("express");
 var express = require("express");
 var app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    next();
-});
-
+app.use(express.json());
+app.use(express.urlencoded());
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017";
 app.listen(8888);
-
 console.log("Serveur démarré")
 const client = new MongoClient(url);
 
-async function main() {
+async function main() //principe de promesse
+{
 	client.connect()
-    .then(
-        client => {
-			db = client.db("COVOITURACE");
-		
+    .then( //apeller apre la réponse du serveur
+        client => { return client.db("COVOITURACE"); } //client = objet js qui correpond Ã  la base de donnÃ©e
+    )
+    .then(async (db) => {
+		console.log("Liste des collections :");
+		let collections = await db.listCollections().toArray();
+		for (let collection of collections) {
+			console.log(collection.name);
+		}
+		return db;
+	})
+	.then((db) => {
 		app.get("/utilisateurs", async (req, res) => {
 			console.log("/utilisateurs");
 			let documents = await db.collection("utilisateurs").find().toArray();
 			res.json(documents);
 		});
 
+		app.post("/inscription", async (req,res) => {
+			console.log("/inscription de ", req.body);
+			let document = await db.collection("utilisateurs").find(req.body).toArray();
+			if( document.length == 1){
+				res.json({"resultat" : 0, "message": "utilisateur déjà existant"});
+			}
+		});
+		
 		app.get("/produits/:type", async (req, res) => {
 			console.log("/produits/"+req.params.type);
 			let documents = await db.collection("produits").find({type:req.params.type}).toArray();
 			res.json(documents);
 		});
 
-		app.get("/categories", async (req,res) => {
-			console.log("/categories");
-			categories = [];
-			let documents = await db.collection("produits").find().toArray();
-			for (let doc of documents) {
-				if (!categories.includes(doc.type)) categories.push(doc.type); 
-			}
-			res.json(categories);
-		});
-	
-		app.post("/user/connexion", async (req,res) => {
-			console.log("/user/connexion avec ", req.body);
-			let document = await db.collection("users").find(req.body).toArray();
-			if (document.length == 1)
-				res.json({"resultat": 1, "message": "Authentification réussie"});
-			else res.json({"resultat": 0, "message": "Email et/ou mot de passe incorrect"});
-		});
 	});
 }
 main();

1ac67f2a62e77f31d934a253858d4a64a3b895f6
LoloLeMono <rouquairolr@gmail.com>
2023-05-07 18:00:56 +0200

    Add route trajet utilisateur

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -9,11 +9,42 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ProfileComponent implements OnInit{
 
+  nom !: string | null;
+  prenom !: string;
+  email !: string;
+  tel !: string;
+  motDePasse !: string ;
   utilisateur !: Utilisateur | null;
 
   constructor(private authService: AuthService){}
 
   ngOnInit(): void {
     this.utilisateur = this.authService.getUtilisateur();
+    this.nom = this.utilisateur ? this.utilisateur.nom : '' ;
+    this.prenom = this.utilisateur ? this.utilisateur.prenom : '' ;
+    this.email = this.utilisateur ? this.utilisateur.email : '' ;
+
+  }
+
+  onSubmit(){
+    if (!this.nom || !this.prenom || !this.email || !this.tel) {
+      alert('Veuillez remplir tous les champs');
+      return;
+    }else if(false){
+      alert('Les mots de passe ne correspondent pas');
+      return;
+    }
+
+    const utilisateur: Utilisateur = {
+      email: this.email,
+      motDePasse: this.motDePasse,
+      nom: this.nom,
+      prenom: this.prenom,
+      telephone: this.tel,
+      numPermis: '',
+      numImmatriculation: '',
+      vitMoyenne: 0
+    }
+
   }
 }
